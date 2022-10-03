55a00dbee0fa52f025c86f2f3aa526fb1e5a638d
Mariko Tanikawa <adoxzmb@gmail.com>
2022-10-04 04:19:27 +0900

    Add: TOP画面追加

--- pages/client/login.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- クライアント用ログイン画面 -->
-  <div>
-    <OLogin user-type="client" />
-  </div>
-</template>
-<script setup lang="ts">
-</script>
--- pages/manager/login.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- ケアマネージャー用ログイン画面 -->
-  <div>
-    <OLogin user-type="manager" />
-  </div>
-</template>
-<script setup lang="ts">
-</script>
