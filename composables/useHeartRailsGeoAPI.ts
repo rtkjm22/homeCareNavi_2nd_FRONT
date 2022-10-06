295d7f60d5f7138315f272edaf890fa9d6b04bec
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-10-06 23:07:00 +0900

    Add: HeartRailsGeoAPIを用いた位置情報取得メソッド作成

---
+++ composables/useHeartRailsGeoAPI.ts
@@ -0,0 +1,65 @@
+/** HeartRailsGeoAPIの共通URL */
+const BASE_URL = 'http://geoapi.heartrails.com/api/json'
+
+/**
+ * レスポンスの共通型。{ response: }は共通で、成功時はメソッド特有の値が、失敗時はerror: stringが返ってくる
+ * @example
+ * //成功時
+ *  { response: [ { location: { city: 札幌市北区, town: 北十条西... } } ] }
+ * //失敗時
+ *  { response: { error: "Cities of postal code:'1628804' do not exist." } }
+ * */
+type BaseResponse<T> = {
+  response: {
+    [K in keyof T]: T[K];
+  } | {
+    error: string;
+  }
+}
+
+/** APIから返ってくる値に共通する住所等の値 */
+type AddressResult = {
+  prefecture: string;
+  city: string;
+  city_kana: string;
+  town: string;
+  town_kana: string;
+  x: number;
+  y: number;
+  postal: string;
+}
+
+/** 郵便番号から都道府県、市区町村、経度緯度等を取得しそれを返す。ヒットしなければundefinedを返す。 */
+const searchByPostal = async (postal: string) => {
+  type Result = { location: AddressResult[] }
+  const { response } = await $fetch<BaseResponse<Result>>(`${BASE_URL}/?method=searchByPostal&postal=${postal}`)
+
+  if ('error' in response) { return }
+  return response.location[0]
+}
+
+/** 郵便番号から最寄り駅を取得しそれを返す。ヒットしなければundefinedを返す。 */
+const getStations = async (postal: string) => {
+  type Result = {
+    station: Array<{
+      name: string;
+      distance: number;
+    } & AddressResult>
+  }
+  const { response } = await $fetch<BaseResponse<Result>>(`${BASE_URL}/?method=getStations&postal=${postal}`)
+
+  if ('error' in response) { return }
+
+  return response.station[0]
+}
+
+/**
+ * 郵便番号・住所・緯度経度データをHeartRailsGeoAPIから取得する。
+ * @see http://geoapi.heartrails.com/api.html
+ */
+export const useHeartRailsGeoAPI = () => {
+  return {
+    searchByPostal,
+    getStations
+  }
+}
