321c45d100dde1de7954a636638f6850800eaae4
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-10-28 00:56:27 +0900

    Fix: 表示崩れ修正

--- pages/manager/auth/clients/index.vue
+++ pages/manager/auth/clients/index.vue
@@ -3,9 +3,7 @@
     <OUserInfo class="wrapper" />
   </div>
 </template>
-<script setup lang="ts">
-definePageMeta({ layout: 'manager' })
-</script>
+
 <style scoped lang="scss">
 .wrapper {
   @apply block pt-4 px-2.5 mb-[52px] w-full max-w-[722px] sm:pt-10 sm:mb-20 md:mx-auto;
--- pages/password-reset/index.vue
+++ pages/password-reset/index.vue
@@ -1,37 +1,39 @@
 <template>
-  <div
-    class="mb-[50px] mt-2 sm:mt-10 sm:mb-20 mx-auto pt-4 flex justify-center max-w-pcCol1 bg-white relative rounded"
-  >
-    <div class="w-full px-4">
-      <div class="mb-6 sm:mb-12 sm:mt-4 sm:text-center">
-        <ATitle ttl-text="パスワードリセット" />
-      </div>
+  <div>
+    <div
+      class="mb-[50px] mt-2 sm:mt-10 sm:mb-20 mx-auto pt-4 flex justify-center max-w-pcCol1 bg-white relative rounded"
+    >
+      <div class="w-full px-4">
+        <div class="mb-6 sm:mb-12 sm:mt-4 sm:text-center">
+          <ATitle ttl-text="パスワードリセット" />
+        </div>
 
-      <form class="mx-auto sm:w-[520px] mb-2" @submit.prevent="sendEmail">
-        <AInput
-          v-model="email"
-          label-for="inputMail"
-          label-text="メールアドレス"
-          input-type="email"
-          placeholder="例) homecarenavi@mail.com"
-          pattern="^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$"
-          :required="true"
-          class="mb-8"
-        />
+        <form class="mx-auto sm:w-[520px] mb-2" @submit.prevent="sendEmail">
+          <AInput
+            v-model="email"
+            label-for="inputMail"
+            label-text="メールアドレス"
+            input-type="email"
+            placeholder="例) homecarenavi@mail.com"
+            pattern="^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$"
+            :required="true"
+            class="mb-8"
+          />
 
-        <AButtonSubmit
-          inner-text="パスワード再設定メール送信"
-          user-type="client"
-          class="py-3 sm:py-4"
-        />
-      </form>
-      <div class="mb-8 sm:mb-16 text-center">
-        <NuxtLink
-          to="/client/login"
-          class="text-sm text-pink"
-        >
-          リセットせずにもどる
-        </NuxtLink>
+          <AButtonSubmit
+            inner-text="パスワード再設定メール送信"
+            user-type="client"
+            class="py-3 sm:py-4"
+          />
+        </form>
+        <div class="mb-8 sm:mb-16 text-center">
+          <NuxtLink
+            to="/client/login"
+            class="text-sm text-pink"
+          >
+            リセットせずにもどる
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </div>
