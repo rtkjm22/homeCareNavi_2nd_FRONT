a3300befcc73b9650fb4f1e088890b6c2efb9d68
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-12-22 13:39:14 +0900

    Add: マネージャー側の予約状況確認機能追加

--- pages/manager/auth/reserves/index.vue
+++ pages/manager/auth/reserves/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="w-[355px] md:w-[730px] m-auto">
-    <ATitle ttl-text="予約状況確認" class="my-4" />
-    <OReserveList class="mb-[61px]" />
+    <div v-if="pending" class="flex justify-center mt-10">
+      <ASpinner size="lg" />
+    </div>
+
+    <template v-else>
+      <ATitle ttl-text="予約状況確認" class="my-4" />
+      <p v-if="reserves == null || reserves.length === 0">
+        予約はありません
+      </p>
+      <OReserveList v-else v-bind="{ reserves }" class="mb-[61px]" />
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-</script>
+const { $api } = useNuxtApp()
 
-<style scoped lang="scss">
-</style>
+const { data: reserves, pending } = useAsyncData(
+  () => $api.client.api.v1.manager.reserves.$get()
+)
+</script>
